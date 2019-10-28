403dac8c439820154c7e85413e38a21db64afa3b
Yash <ythacker@wustl.edu>
2019-10-28 00:38:38 -0500

    adding cooking function (not complete)

---
+++ Assets/Script/CounterInteration.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CounterInteration : MonoBehaviour
+{
+    bool pressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pressed && GetComponent<Character>() != null)
+        {
+            if (Vector3.Distance(GetComponent<GameItemController>().mainCharacter.transform.position, transform.position) < GetComponent<GameItemController>().minDistance) 
+            {
+                pressed = false; // Resetting pressed
+                beginCooking();
+            }
+
+        }
+    }
+
+
+
+
+    public void beginCooking()
+    {
+        GameObject item1 = GetComponent<BackpackController>().slot1;
+        GameObject item2 = GetComponent<BackpackController>().slot2;
+        GameObject item3 = GetComponent<BackpackController>().slot3;
+
+        print(item1);
+
+    }
+}
