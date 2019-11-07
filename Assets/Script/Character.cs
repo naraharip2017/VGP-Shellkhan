28681377332d4b73f64970c4d16652e00d4be21a
Pavan Narahari <naraharip@wustl.edu>
2019-11-07 00:15:15 -0600

    canvas menu added

--- Assets/Script/Character.cs
+++ Assets/Script/Character.cs
@@ -25,38 +25,43 @@ public class Character : MonoBehaviour
     void Update()
     {
 
-        if (Input.GetKey(KeyCode.W))
+        if (GameTimer.round)
         {
-            transform.position += new Vector3(0, walkSpeed, 0);
-            target = transform.position;
-            setWalkAnimation("WalkUp");
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            transform.position += new Vector3(0, -walkSpeed, 0);
-            target = transform.position;
-            setWalkAnimation("WalkDown");
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            transform.position += new Vector3(-walkSpeed, 0, 0);
-            target = transform.position;
-            setWalkAnimation("WalkLeft");
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += new Vector3(walkSpeed, 0, 0);
-            target = transform.position;
-            setWalkAnimation("WalkRight");
-        }
-        else
-        {
-            animator.SetBool("WalkUp", false);
-            animator.SetBool("WalkDown", false);
-            animator.SetBool("WalkLeft", false);
-            animator.SetBool("WalkRight", false);
 
 
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position += new Vector3(0, walkSpeed, 0);
+                target = transform.position;
+                setWalkAnimation("WalkUp");
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                transform.position += new Vector3(0, -walkSpeed, 0);
+                target = transform.position;
+                setWalkAnimation("WalkDown");
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                transform.position += new Vector3(-walkSpeed, 0, 0);
+                target = transform.position;
+                setWalkAnimation("WalkLeft");
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.position += new Vector3(walkSpeed, 0, 0);
+                target = transform.position;
+                setWalkAnimation("WalkRight");
+            }
+            else
+            {
+                animator.SetBool("WalkUp", false);
+                animator.SetBool("WalkDown", false);
+                animator.SetBool("WalkLeft", false);
+                animator.SetBool("WalkRight", false);
+
+
+            }
         }
 
 
